601bbf941b2e45fc41d2813b8cf288c80b7a195c
refactoredjello <gil.mandler@gmail.com>
2017-11-02 16:00:02 -0400

    Basic HTML structure

--- client/App.js
+++ client/App.js
@@ -1,13 +1,25 @@
 class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      ronSays: null,
+      url: null,
+      response: null,
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Ron's Words...</h1>
+          <h1 className="App-title">What does ron think?</h1>
         </header>
         <p className="App-intro">
-         It's Alive! 
+         Enter url to get a summary of the text. Then see how ron swanson feels about it.
         </p>
+        <div className="">
+          <input className="form-control" type="text" />
+          <button className="submit">Analyze Text</button>
+        </div>
       </div>
     );
   }
